f836d1037208731573d37b1d1db097849a829090
MRxRafi <rafadriveclase@gmail.com>
2021-01-17 00:20:37 +0100

    Fixed lint error

--- src/app/landscape/landscape.component.ts
+++ src/app/landscape/landscape.component.ts
@@ -6,8 +6,8 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./landscape.component.css']
 })
 export class LandscapeComponent implements OnInit {
-  @Input('header') header: string;
-  @Input('subheader') subheader: string;
+  @Input() header: string;
+  @Input() subheader: string;
   constructor() { }
 
   ngOnInit(): void {
