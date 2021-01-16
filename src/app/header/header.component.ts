c10e26310071efbe3df6ed9a2c5f53c8254250e3
MRxRafi <rafadriveclase@gmail.com>
2021-01-16 23:09:17 +0100

    Collapse navbar when click link & improved navbar model

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavbarModel} from './navbar.model';
+import {NavbarModel, headers} from './navbar.model';
 
 @Component({
   selector: 'app-header',
@@ -10,26 +10,7 @@ export class HeaderComponent {
   headers: NavbarModel[];
   lastActiveLink = 0;
   constructor() {
-    this.headers = [
-      {
-        routerlink: 'home',
-        headerDisplay: 'Home',
-        active: true,
-        icon: 'fas fa-home'
-      },
-      {
-        routerlink: 'skills',
-        headerDisplay: 'Habilidades',
-        active: false,
-        icon: 'fas fa-book'
-      },
-      {
-        routerlink: 'contact',
-        headerDisplay: 'Contacto',
-        active: false,
-        icon: 'fas fa-phone'
-      },
-    ];
+    this.headers = headers;
   }
   changeHeader(clickedHeader): void {
     this.headers[this.lastActiveLink].active = false;
--- src/app/header/navbar.model.ts
+++ src/app/header/navbar.model.ts
@@ -4,3 +4,24 @@ export interface NavbarModel {
   active: boolean;
   icon?: string;
 }
+
+export const headers: NavbarModel[] = [
+  {
+    routerlink: 'home',
+    headerDisplay: 'Inicio',
+    active: true,
+    icon: 'fas fa-home'
+  },
+  {
+    routerlink: 'skills',
+    headerDisplay: 'Habilidades',
+    active: false,
+    icon: 'fas fa-book'
+  },
+  {
+    routerlink: 'contact',
+    headerDisplay: 'Contacto',
+    active: false,
+    icon: 'fas fa-phone'
+  }
+];
