0b100757dd2165facb68bc02ec3a998aea792ada
MRxRafi <rafadriveclase@gmail.com>
2021-01-17 19:40:52 +0100

    First try of fixing coverage tests. Added dependency to transform karma coverage to Generic Coverage xml format

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-sonarqube-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -32,7 +33,25 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    sonarqubeReporter: {
+      basePath: 'src/app',        // test files folder
+      filePattern: '**/*spec.ts', // test files glob pattern
+      encoding: 'utf-8',          // test files encoding
+      outputFolder: 'reports',    // report destination
+      legacyMode: false,          // report for Sonarqube < 6.2 (disabled)
+      reportName: (metadata) => { // report name callback, but accepts also a
+        // string (file name) to generate a single file
+        /**
+         * Report metadata array:
+         * - metadata[0] = browser name
+         * - metadata[1] = browser version
+         * - metadata[2] = plataform name
+         * - metadata[3] = plataform version
+         */
+        return metadata.concat('xml').join('.');
+      }
+    },
+    reporters: ['progress', 'kjhtml', 'sonarqube'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
