70530454cb9f7e3a459ec591185ec87615c9eba9
MRxRafi <rafadriveclase@gmail.com>
2021-01-24 14:12:14 +0100

    Modified Home page to implement ngFor in Studies section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {STUDIES, StudyRow} from './studies.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeComponent {
+  studies: StudyRow[];
+  constructor() {
+    this.studies = STUDIES;
   }
-
 }
